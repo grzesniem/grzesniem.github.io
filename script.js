6920626111d4a910a22887ace717890687d53a77
grzesniem <49124909+grzeg2010@users.noreply.github.com>
2020-06-26 23:38:50 +0200

    usprawnienia
    transition
    ikony

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 var x = 0;
 var center_circle = document.getElementById("center");
+var rotor = document.getElementById("rot");
+
 //Nawigacja do przodu ➡
 //Przejście między ostatnim i pierwszym punktem
 function zerowanie_przod() {
     x = 0;
-    document.getElementById("rot").style.transition = "0s";
-    rotation();
-    document.getElementById("rot").removeEventListener("transitionend", zerowanie_przod);
+    rotor.style.transition = "0s";
+    rotor.style.transform = "translateX(-50%) rotate(" + x * -45 + "deg) ";
+    rotor.removeEventListener("transitionend", zerowanie_przod);
 }
 
 //Nawigacja do przodu
 function przod() {
-    document.getElementById("rot").style.transition = "1s";
+    //rotor.style.transition = "1s";
     switch (x) {
         case 0:
             x = 1;
@@ -37,30 +39,33 @@ function przod() {
         case 7:
             x = 8;
             rotation();
-            document.getElementById("rot").addEventListener("transitionend", zerowanie_przod);
+            center_circle.style.backgroundImage = "url('images/samolot.jpg')";
+            rotor.addEventListener("transitionend", zerowanie_przod);
 
             break;
     }
     rotation();
+    center_image();
 }
 
 //Nawigacja do tyłu ⬅
 //Przejście między pierwszym i ostatnim punktem
 function zerowanie_tyl() {
     x = 7;
-    document.getElementById("rot").style.transition = "0s";
-    rotation();
-    document.getElementById("rot").removeEventListener("transitionend", zerowanie_tyl);
+    rotor.style.transition = "0s";
+    rotor.style.transform = "translateX(-50%) rotate(" + x * -45 + "deg) ";
+    rotor.removeEventListener("transitionend", zerowanie_tyl);
 }
 
 //Nawigacja do tyłu
 function tyl() {
-    document.getElementById("rot").style.transition = "1s";
+    //rotor.style.transition = "1s";
     switch (x) {
         case 0:
             x = -1;
             rotation();
-            document.getElementById("rot").addEventListener("transitionend", zerowanie_tyl);
+            center_circle.style.backgroundImage = "url('images/autobus.jpg')";
+            rotor.addEventListener("transitionend", zerowanie_tyl);
             break;
         case 1:
             x = 0;
@@ -85,8 +90,75 @@ function tyl() {
             break;
     }
     rotation();
+    center_image();
 }
 
+//Funckja odpowiedzialna za obrót koła
+function rotation() {
+    rotor.style.transition = "1s";
+    rotor.style.transform = "translateX(-50%) rotate(" + x * -45 + "deg) ";
+    /*if (x == 8) {
+        document.getElementById("center").innerHTML = 1;
+    } else if (x == -1) {
+        document.getElementById("center").innerHTML = 8;
+    } else {
+        document.getElementById("center").innerHTML = x + 1;
+    }*/
+    window.clearTimeout(this.timeoutID);
+    this.timeoutID = window.setTimeout(przod, 8000);
+}
+
+//Akcje po załadowaniu strony
+function zaladowana() {
+    center_image();
+    document.getElementById("rot").style.transition = "1s";
+    document.getElementById("head").innerHTML += "<style>.bubble{transition:0.1s}#center{transition: 0.5s}</style>";
+    rotation();
+
+    //Nawigacja przez naciśnięcie na kółka
+    document.getElementById("rotA").addEventListener("click", function () {
+        x = 0;
+        rotation();
+        center_image();
+    });
+    document.getElementById("rotB").addEventListener("click", function () {
+        x = 1;
+        rotation();
+        center_image();
+    });
+    document.getElementById("rotC").addEventListener("click", function () {
+        x = 2;
+        rotation();
+        center_image();
+    });
+    document.getElementById("rotD").addEventListener("click", function () {
+        x = 3;
+        rotation();
+        center_image();
+    });
+    document.getElementById("rotE").addEventListener("click", function () {
+        x = 4;
+        rotation();
+        center_image();
+    });
+    document.getElementById("rotF").addEventListener("click", function () {
+        x = 5;
+        rotation();
+        center_image();
+    });
+    document.getElementById("rotG").addEventListener("click", function () {
+        x = 6;
+        rotation();
+        center_image();
+    });
+    document.getElementById("rotH").addEventListener("click", function () {
+        x = 7;
+        rotation();
+        center_image();
+    });
+}
+
+//Obrazy na środku
 function center_image() {
     switch (x) {
         case 0:
@@ -108,68 +180,10 @@ function center_image() {
             center_circle.style.backgroundImage = "url('images/statek.jpg')";
             break;
         case 6:
-            center_circle.style.backgroundImage = "url('images/samolot.jpg')";
+            center_circle.style.backgroundImage = "url('images/rower.jpg')";
             break;
         case 7:
-            center_circle.style.backgroundImage = "url('images/samolot.jpg')";
+            center_circle.style.backgroundImage = "url('images/autobus.jpg')";
             break;
     }
-}
-
-//Funckja odpowiedzialna za obrót koła
-function rotation() {
-
-    document.getElementById("rot").style.transform = "translateX(-50%) rotate(" + x * -45 + "deg) ";
-    /*if (x == 8) {
-        document.getElementById("center").innerHTML = 1;
-    } else if (x == -1) {
-        document.getElementById("center").innerHTML = 8;
-    } else {
-        document.getElementById("center").innerHTML = x + 1;
-    }*/
-    window.clearTimeout(this.timeoutID);
-    this.timeoutID = window.setTimeout(przod, 8000);
-    center_image();
-}
-
-//Akcje po załadowaniu strony
-function zaladowana() {
-    center_image();
-    document.getElementById("rot").style.transition = "1s";
-    document.getElementById("head").innerHTML += "<style>.bubble{transition:0.1s}#center{transition: 0.5s}</style>";
-    window.setTimeout(przod, 8000);
-
-    //Nawigacja przez naciśnięcie na kółka
-    document.getElementById("rotA").addEventListener("click", function () {
-        x = 0;
-        rotation();
-    });
-    document.getElementById("rotB").addEventListener("click", function () {
-        x = 1;
-        rotation();
-    });
-    document.getElementById("rotC").addEventListener("click", function () {
-        x = 2;
-        rotation();
-    });
-    document.getElementById("rotD").addEventListener("click", function () {
-        x = 3;
-        rotation();
-    });
-    document.getElementById("rotE").addEventListener("click", function () {
-        x = 4;
-        rotation();
-    });
-    document.getElementById("rotF").addEventListener("click", function () {
-        x = 5;
-        rotation();
-    });
-    document.getElementById("rotG").addEventListener("click", function () {
-        x = 6;
-        rotation();
-    });
-    document.getElementById("rotH").addEventListener("click", function () {
-        x = 7;
-        rotation();
-    });
 }
